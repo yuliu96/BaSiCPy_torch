f5fecd670bdbaeea6083d2815a7d79d00090fffa
Yu Liu <liuyu9671@gmail.com>
2025-03-23 22:25:46 +0100

    Update __init__.py

--- basicpy/__init__.py
+++ basicpy/__init__.py
@@ -1,16 +1,15 @@
-# -*- coding: utf-8 -*-
+"""Compute an illumination correction model for mosaic microscope images."""
 
-"""Top-level package for LSFM FUSE."""
+import logging
+import os
 
-__author__ = "Yu Liu"
-__email__ = "liuyu9671@gmail.com"
-# Do not edit this string manually, always use bumpversion
-# Details in CONTRIBUTING.md
-__version__ = "0.0.1"
+from basicpy import datasets
+from basicpy.basicpy import BaSiC
+
+# Set logger level from environment variable
+logging_level = os.getenv("BASIC_LOG_LEVEL", default="WARNING").upper()
+logger = logging.getLogger(__name__)
+logger.setLevel(logging_level)
 
 
-def get_module_version():
-    return __version__
-
-
-from .basicpy import BaSiC
+__all__ = ["BaSiC", "datasets", "metrics"]
